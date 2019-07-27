d6a8677dad7d9ac72dc7463dbf13d99b7be670a2
christian512 <c.staufe@posteo.net>
2019-07-27 18:54:52 +0200

    add check for winner function

--- ThreeMensMorris/board.py
+++ ThreeMensMorris/board.py
@@ -8,20 +8,21 @@ class Board:
         self.phase = 0
 
     def __str__(self):
-        out = str(self.board_array[0]) + '------' + str(self.board_array[1]) + '------' + str(self.board_array[2]) + '\n' \
+        out = '\n \n' + str(self.board_array[0]) + '------' + str(self.board_array[1]) + '------' + str(self.board_array[2]) + '\n' \
             + '|      |      |\n' \
             + str(self.board_array[3]) + '------' + str(self.board_array[4]) + '------' + str(self.board_array[5]) + '\n' \
             + '|      |      |\n' \
-            + str(self.board_array[6]) + '------' + str(self.board_array[7]) + '------' + str(self.board_array[8]) + '\n'
+            + str(self.board_array[6]) + '------' + str(self.board_array[7]) + '------' + str(self.board_array[8]) + '\n \n'
         return out
 
     def place_marker(self,pos, player_num):
         # Check if position is vacant
         if self.board_array[pos] == 0:
-            self.board_array[pos] == player_num
+            self.board_array[pos] = player_num
         else:
             print('Position already set on board!')
             return False
+        self.check_mill()
         return True
 
     def move_marker(self,FROM, TO):
@@ -34,8 +35,27 @@ class Board:
             return False
         # change markers
         self.board_array[TO],self.board_array[FROM] = self.board_array[FROM],self.board_array[TO]
+        self.check_mill()
         return True
+
+    def check_mill(self):
+        neighbors = [
+        [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8]
+        ]
+        for neighbor in neighbors:
+            if self.board_array[neighbor].all() == 1:
+                print(self)
+                sys.exit('Player number 1 wins!')
+            if self.board_array[neighbor].all() == 2:
+                print(self)
+                sys.exit('Player number 2 wins!')
 
 if __name__ == '__main__':
     b = Board()
+    print(b)
+    b.place_marker(pos=0,player_num=1)
+    b.place_marker(pos=1,player_num=1)
+    b.place_marker(pos=2,player_num=1)
+    print(b)
+
     print(b)
